1025698d432bdba1f91b752ec9dbfeb787278464
Sammy Jackson <sam.jackson@live.ca>
2020-03-05 02:33:43 -0500

    Updates

---
+++ precache-manifest.6f5d640e18d08311778923adf9639077.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77759d2cf7c43e6f4f5691bb7b27c6d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c446801f7e9a5cd75cbc",
+    "url": "/static/css/main.d6b895ba.chunk.css"
+  },
+  {
+    "revision": "f794dce58452e894215a",
+    "url": "/static/js/2.09cb7448.chunk.js"
+  },
+  {
+    "revision": "4e73e17a27979cfbb811eb2b5d96f523",
+    "url": "/static/js/2.09cb7448.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c446801f7e9a5cd75cbc",
+    "url": "/static/js/main.230a4fc0.chunk.js"
+  },
+  {
+    "revision": "3877348275e8a43c71af",
+    "url": "/static/js/runtime-main.9270ea39.js"
+  },
+  {
+    "revision": "a397d89c44b9bd679ffe17cfe4a0dcde",
+    "url": "/static/media/bahnschrift.a397d89c.ttf"
+  },
+  {
+    "revision": "5fbae7a148389a502bc8fedbfc66b8af",
+    "url": "/static/media/micross.5fbae7a1.ttf"
+  }
+]);
